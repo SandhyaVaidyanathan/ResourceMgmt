67d7e6e5e9a98d2f2df46b4588f521d3400a4c2a
SandhyaVaidyanathan <o2-vaidyanathan@hoare7.cs.umsl.edu>
2017-11-16 04:00:09 -0600

    setting up necessary shared memory

--- oss.c
+++ oss.c
@@ -20,20 +20,28 @@
 void spawnSlaveProcess(int);
 void interruptHandler(int);
 void clearSharedMem1();
+void clearSharedMem2();
+void clearResMem();
+void clearMsg();
 
 pid_t childpid;
 int spawnedSlaves = 0;
 int mypid = 0;
 char* arg1;
-int shmid;
+int shmid,shmpcbid, resid, msgid;
 shmClock *shinfo;
+shmPcb *shpcbinfo;
+shmRes *shresinfo;
+msgholder *msgqinfo;
+
+FILE *fp;
 
 int main(int argc, char const *argv[])
 {
+	int status = 0;
 	char* logfile;
 	logfile = "log.txt";
-	key_t clock_key;
-
+	key_t clock_key, pcb_key,res_key,msg_key;
 
 //signal handling 
 	signal(SIGINT, interruptHandler); 
@@ -41,7 +49,9 @@ int main(int argc, char const *argv[])
 	alarm(2);	// 2 real seconds
 
 	clock_key = 555;
-	//pcb_key = 666;
+	pcb_key = 666;    
+	res_key = 777;
+	msg_key =888; 
 	//Create shared memory segment for clock
 	shmid = shmget(clock_key, 40*sizeof(shinfo), 0766 |IPC_CREAT |IPC_EXCL);
 	if ((shmid == -1) && (errno != EEXIST)) /* real error */
@@ -65,6 +75,73 @@ int main(int argc, char const *argv[])
 	//	shinfo->scheduledpid = -1;
 	}
 
+	//create shared memory for pcb
+	shmpcbid = shmget(pcb_key, 40*sizeof(shpcbinfo), 0766 |IPC_CREAT |IPC_EXCL);
+	if ((shmpcbid == -1) && (errno != EEXIST)) /* real error */
+	{
+		perror("Unable to create shared memory");
+		return -1;
+	}
+	if (shmpcbid == -1)
+	{
+		printf("Shared Memory Already created");
+		return -1;
+	}
+	else
+	{
+		shpcbinfo = (shmPcb*)shmat(shmpcbid,NULL,0);
+		if (shpcbinfo == (void*)-1)
+			return -1;
+		// clock initially set to 0
+		shpcbinfo->pcbId = 0;
+		shpcbinfo->parrivalsec = 0; // creation time 
+		shpcbinfo->parrivalnsec = 0;
+		shpcbinfo->request = -1;
+		shpcbinfo->release = -1;
+		shpcbinfo->deadlock = 0;
+
+	}
+//Create shared memory for resources
+	resid = shmget(res_key, 40*sizeof(shresinfo), 0766 |IPC_CREAT |IPC_EXCL);
+	if ((resid == -1) && (errno != EEXIST)) /* real error */
+	{
+		perror("Unable to create shared memory");
+		return -1;
+	}
+	if (resid == -1)
+	{
+		printf("Shared Memory Already created");
+		return -1;
+	}
+	else
+	{
+		shresinfo = (shmRes*)shmat(resid,NULL,0);
+		if (shresinfo == (void*)-1)
+			return -1;
+		shresinfo->quantity = 0;
+		shresinfo->quantityAvail = 0;
+	}
+
+
+if((msgid = msgget(msg_key, IPC_CREAT | 0777)) == -1) {
+    perror("Master msgget for master queue");
+    exit(-1);
+  }
+
+//File open 
+	fp = fopen(logfile, "w");
+	fprintf(fp, "Opening file ... \n" );
+
+//clearing memory
+	free(arg1);
+	wait(&status);
+	clearSharedMem1();
+	clearSharedMem2();
+	clearResMem();
+	clearMsg();
+	fclose(fp);
+	//kill(-getpgrp(),SIGQUIT);
+
 	return 0;
 }
 
@@ -83,6 +160,12 @@ void interruptHandler(int SIG){
     fprintf(stderr, "Master has timed out. killing processes\n");
   }
 
+  	kill(-getpgrp(), 9);
+  	clearSharedMem1();
+  	clearSharedMem2();
+  	clearResMem();
+  	clearMsg();
+
   }
   void clearSharedMem1()
 {
@@ -98,7 +181,37 @@ void interruptHandler(int SIG){
 	}
 }
 
+void clearSharedMem2()
+{
+	int error = 0;
+	if(shmdt(shpcbinfo) == -1) {
+		error = errno;
+	}
+	if((shmctl(shmpcbid, IPC_RMID, NULL) == -1) && !error) {
+		error = errno;
+	}
+	if(!error) {
+		return ;
+	}
+}
+void clearResMem()
+{
+		int error = 0;
+	if(shmdt(shresinfo) == -1) {
+		error = errno;
+	}
+	if((shmctl(resid, IPC_RMID, NULL) == -1) && !error) {
+		error = errno;
+	}
+	if(!error) {
+		return ;
+	}
+}
 
+void clearMsg()
+{
+  msgctl(msgid, IPC_RMID, NULL);
+}
 void spawnSlaveProcess(int noOfSlaves)
 {
 	int i;
--- shm.h
+++ shm.h
@@ -2,3 +2,24 @@ typedef struct osClock {
     unsigned long sec;
     unsigned long nsec;
 }shmClock;
+
+typedef struct pcb {
+  pid_t pcbId;
+  unsigned long parrivalsec; //time when process is forked
+  unsigned long parrivalnsec;
+  int request;
+  int release;
+  int deadlock;
+  int terminate;
+  int resources[20]; //no of resources
+} shmPcb;
+
+typedef struct resource {
+  int quantity;
+  int quantityAvail;
+} shmRes;
+
+typedef struct msgholder {
+  long mType;
+  char mText[100];
+} msgholder;
